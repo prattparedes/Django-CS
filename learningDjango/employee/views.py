254f1ecd876b69338a083bb39e4bec9577efd899
Pratt <pratt.paredes@gmail.com>
2022-12-15 23:15:06 -0500

    delete and update functionality

--- learningDjango/employee/views.py
+++ learningDjango/employee/views.py
@@ -18,13 +18,39 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def create(request):
     template = loader.get_template('createPage.html')
     return HttpResponse(template.render({}, request))
+
 
 def createData(request):
     data1 = request.POST['name']
     data2 = request.POST['title']
     newEmployee = Employee(name=data1, title=data2)
     newEmployee.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    deleteEmployee = Employee.objects.get(id=id)
+    deleteEmployee.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def update(request, id):
+    updateEmployee = Employee.objects.get(id=id)
+    template = loader.get_template('updatePage.html')
+    context = {
+        'Employee': updateEmployee
+    }
+    return HttpResponse(template.render(context, request))
+
+def updateData(request, id):
+    name = request.POST['name']
+    title = request.POST['title']
+    updateEmployee = Employee.objects.get(id=id)
+    updateEmployee.name = name
+    updateEmployee.title = title
+    updateEmployee.save()
     return HttpResponseRedirect(reverse('index'))
