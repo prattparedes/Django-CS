40a8925e33f8d696f3269eb7536ada672b62bc95
Pratt <pratt.paredes@gmail.com>
2022-12-17 21:22:01 -0500

    create superuser, admintable

--- learningDjango/employee/models.py
+++ learningDjango/employee/models.py
@@ -7,6 +7,9 @@ class Employee(models.Model):
     name = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name},{self.title}"
+
 # create Table 'employee_employee' (
 #     'id' Int NOT NULL PRIMARY KEY AUTOINCREMENT,
 #     'name': varchar(255) NOT NULL,
